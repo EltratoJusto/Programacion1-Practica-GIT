db367ba4e8970976e0e60d22554842d2660db171
EltratoJusto <hernanstriebeck88@gmail.com>
2024-07-24 20:54:29 -0300

    Función suma basica
    Prueba de versión suma básica

--- Practica_basicas_programacion1/main.c
+++ Practica_basicas_programacion1/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*FUNCION SUMA*/
+int suma(int a, int b)
+{
+int suma=0;
+
+    suma=a+b;
+
+return suma;
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    int a=7;
+    int b=5;
+
+    int resultado_suma=suma(a,b);
+
+    printf("El resultado de la suma es %d", suma),
+
     return 0;
 }
