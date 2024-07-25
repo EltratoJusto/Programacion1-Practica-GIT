c8b3a5501548eda8812958e3d0f77264279d8ca2
EltratoJusto <hernanstriebeck88@gmail.com>
2024-07-24 21:25:29 -0300

    Versión final suma simple
    Se agregó el ingreso de los números a sumar por teclado.

--- Practica_basicas_programacion1/main.c
+++ Practica_basicas_programacion1/main.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*FUNCION SUMA*/
+// Declaracion de la funcion suma
+int suma(int a, int b);
+
+// FUNCION SUMA
 int suma(int a, int b)
 {
-int suma=0;
-
-    suma=a+b;
-
-return suma;
+return a+b;
 }
 
 int main()
 {
-    int a=7;
-    int b=5;
+    int a;
+    int b;
 
+    // Solicitio el ingreso de los numeros a sumar
+    printf("Ingrese el primer numero entero: ");
+    scanf("%d", &a);
+
+    printf("Ingrese el segundo numero entero: ");
+    scanf("%d", &b);
+
+    // Llamo a la funcion y retorno el resultado
     int resultado_suma=suma(a,b);
 
-    printf("El resultado de la suma es %d", resultado_suma);
+    printf("El resultado de la suma de %d y %d es %d.", a, b, resultado_suma);
 
     return 0;
 }
